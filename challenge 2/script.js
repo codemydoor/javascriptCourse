19946fdfd558e089e9809e7d785081cfd157d8d4
Edmund Quashie <dwolino@gmail.com>
2020-04-30 17:10:36 +0000

    update

--- challenge 2/script.js
+++ challenge 2/script.js
@@ -1,6 +1,6 @@
 
 
-let teamJohnScore = 89 + 20 + 103;
+let teamJohnScore = 22289 + 20 + 103;
 let teamMarkScore = 116 + 94 + 123;
 
 let teamJohnScore_Average = teamJohnScore / 2
@@ -11,8 +11,29 @@ console.log(teamMarkScore_Average)
 
 // if (teamJohnScore_Average < teamMarkScore_Average){
 //     console.log("winner")
-// } else{
-//     console.log("loser")
-// }
+// } else if(teamJohnScore_Average > teamMarkScore_Average){
+//     console.log("looser")
+// }else{console.log("draw")}
 
-teamJohnScore_Average < teamMarkScore_Average ? console.log("winner") : console.log("loser")
+
+teamJohnScore_Average < teamMarkScore_Average ? console.log("winner") 
+: teamJohnScore_Average > teamMarkScore_Average ? console.log("loser")
+: console.log('draw')
+
+
+// ______________________________EXTRA _______________________________
+
+
+let teamMaryScore = 97 + 134 + 105
+
+let teamMaryScore_Average = teamMaryScore / 2
+
+if (teamJohnScore_Average>teamMarkScore_Average && teamJohnScore_Average > teamMaryScore_Average){
+    console.log("Team John has won")
+} else if (teamMarkScore_Average > teamJohnScore_Average && teamMarkScore_Average > teamMaryScore_Average){
+    console.log("Team Mark has won")
+} else if (teamMaryScore_Average > teamJohnScore_Average && teamMaryScore_Average > teamMarkScore_Average){
+    console.log("Team Mary has won")
+} else {
+    console.log("it is a draw")
+}
